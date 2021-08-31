369394cd9825d15b20c75aa1b781b81e31e27eb0
ElPiloto <luis.r.piloto@gmail.com>
2021-08-31 14:33:00 +0100

    Initial version of plugin setup and checking for diary files.

---
+++ lua/prompts/init.lua
@@ -0,0 +1,50 @@
+-- luacheck: push ignore 211 (unused variable)
+--local api = vim.api
+
+-- TODO(elpiloto): make debug level based on sidekick option.
+--local log = require('plenary.log').new({ plugin = 'prompts.nvim', level='debug' })
+-- luacheck: pop
+
+local M = {}
+
+M.DATE_AND_EXTENSION = "date_and_extension"
+M.VIMWIKI_FN = "vimwiki_fn"
+
+local call_vimwiki_fn = function(fn_call)
+  local fn_call_cmd = ":echo " .. fn_call
+  local output = vim.api.nvim_exec(fn_call_cmd, true)
+  return output
+end
+
+
+M.is_diary_file = function(fname, method)
+  if method == M.DATE_AND_EXTENSION then
+    local date_ext_pattern = '%d%d%d%d%-%d%d%-%d%d%.(.*)$'
+    local ext_capture = string.match(fname, date_ext_pattern)
+    for _, valid_ext in pairs(vim.g.prompts_valid_diary_extensions) do
+      if ext_capture == valid_ext then
+        print("Valid file.")
+        return true
+      end
+    end
+    return false
+  elseif method == M.VIMWIKI_FN then
+    local cmd = ":echom vimwiki#base#is_diary_file('" .. fname .. "')"
+    local is_diary = vim.api.nvim_exec(cmd, true)
+    return is_diary == "1"
+  end
+end
+
+M.add_prompt_if_empty = function()
+  local fname = vim.fn.expand("<afile>")
+  local method = M.VIMWIKI_FN
+  if vim.g.prompts_manual_diary_check == 1 then
+    method = M.DATE_AND_EXTENSION
+  end
+  local is_diary = M.is_diary_file(fname, method)
+  if is_diary then
+    print("We would add a prompt now!")
+  end
+end
+
+return M
---
+++ plugin/prompts.vim
@@ -0,0 +1,31 @@
+fun! Prompts()
+	lua for k in pairs(package.loaded) do if k:match("^prompts") then package.loaded[k] = nil end end
+	lua require("prompts").run(1)
+endfun
+
+augroup Prompts
+	autocmd!
+augroup END
+
+if !exists('g:prompts_vimwiki_list')
+  " TODO(ElPiloto): Remove writing here.
+  let g:prompts_vimwiki_list = ['diary.*', 'writing.i_wish', 'writing.how']
+endif
+if !exists('g:prompts_enable_vimwiki')
+  let g:prompts_enable_vimwiki = 1
+endif
+if !exists('g:prompts_manual_diary_check')
+  " Controls whether we use our own date and extension check for a diary."
+  " vimwiki provides a vimwiki#base#is_diary_file command, but that only works for opening today's diary.
+  " manually checking allows us to detect yesterday/tomorrow diary entries too.
+  let g:prompts_manual_diary_check = 1
+endif
+" TODO(ElPiloto): Add setting for dict of filetypes --> list of prompt types.
+" let g:prompts_list = {'writing': ['writing.i_wish']}
+if !exists('g:prompts_valid_diary_extensions')
+  " Only used for prompts_manual_diary_check = 1.
+  let g:prompts_valid_diary_extensions = ['wiki', 'md']
+endif
+augroup Prompts
+  autocmd BufCreate */diary/* :lua require('prompts').add_prompt_if_empty()
+augroup END
